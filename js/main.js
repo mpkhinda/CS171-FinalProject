9bf9a01f1cda23f3c8b7237897d264769975038c
Alex Cardelle <acardelle@icloud.com>
2022-11-12 10:52:05 -0500

    Added census_tracts to promises

--- js/main.js
+++ js/main.js
@@ -7,8 +7,11 @@ let myStreetVis;
 
 // load data using promises
 let promises = [
-    d3.json("data/Street_Segments_2005.geojson") // load street segments at [0]
+    d3.json("data/Street_Segments_2005.geojson"), // load street segments at [0]
+
     //add additional datasets below as items in this array and comment what they are
+    d3.json("data/Census_Tracts_in_2020.geojson") // census tract data for DC [1]
+
 ];
 
 Promise.all(promises)
