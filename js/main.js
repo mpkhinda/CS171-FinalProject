8320a2156e57dd17d509ad5b1b6918e9be5b25a7
Alex Cardelle <acardelle@icloud.com>
2022-11-20 23:11:09 -0500

    Updated main.js to call class radarChart with data

--- js/main.js
+++ js/main.js
@@ -7,6 +7,32 @@ let selectedNeighborhood = "NATIONAL MALL"; // set starting neighborhood here
 let myStreetVis;
 let myMapVis;
 
+let radarData = [
+    [//Census Tract 70
+        {axis:"Drive Alone",value:0.28},
+        {axis:"Public Transit",value:0.24},
+        {axis:"Walked",value:0.25},
+        {axis:"Bicycle",value:0.07},
+        {axis:"Taxicab",value:0.01},
+        {axis:"Worked from Home",value:0.14},
+    ],[//Census Tract 1
+        {axis:"Drive Alone",value:0.40},
+        {axis:"Public Transit",value:0.13},
+        {axis:"Walked",value:0.21},
+        {axis:"Bicycle",value:0.08},
+        {axis:"Taxicab",value:0.04},
+        {axis:"Worked from Home",value:0.15},
+    ],[//Census Tract
+        {axis:"Drive Alone",value:0.22},
+        {axis:"Public Transit",value:0.26},
+        {axis:"Walked",value:0.29},
+        {axis:"Bicycle",value:0.10},
+        {axis:"Taxicab",value:0.02},
+        {axis:"Worked from Home",value:0.11},
+    ]
+];
+
+
 // load data using promises
 let promises = [
     d3.json("data/Street_Segments_2005.geojson"), // load street segments at [0]
@@ -17,7 +43,9 @@ let promises = [
     d3.json("data/BikeTripsEnded_by_Neighborhood.geojson"), // bike trips ended at [3]
     d3.json("data/BikeTripsStarted_by_Neighborhood.geojson"), // bike trips started at [4]
     d3.json("data/TaxiTripsEnded_by_Neighborhood.geojson"), // bike trips ended at [5]
-    d3.json("data/TaxiTripsStarted_by_Neighborhood.geojson") // bike trips started at [6]
+    d3.json("data/TaxiTripsStarted_by_Neighborhood.geojson"), // bike trips started at [6]
+    radarData //radarData started at [7]
+
 
 
 
@@ -47,9 +75,11 @@ let eventHandler = {
 function initMainPage(dataArray){
     console.log(dataArray); // explore dataArray in console
 
-    myStreetVis = new StreetVis("street-vis", dataArray[0], d3.geoMercator());
-    myMapVis = new MapVis("map-vis", dataArray[1], dataArray[2], dataArray[3], dataArray[4], dataArray[5], dataArray[6], MapboxToken, eventHandler)
 
+    myStreetVis = new StreetVis("street-vis", dataArray[0], d3.geoMercator());
+    //myMapVis = new MapVis("map-vis", dataArray[1], dataArray[2], dataArray[3], dataArray[4], dataArray[5], dataArray[6], MapboxToken, eventHandler)
+    //Call function to draw the Radar chart
+    myRadarChart = new RadarChart("radar-chart", dataArray[7]);
     //use dataArray indexing to pass specific datasets from the promise to the visualization classes
 
 }
@@ -60,5 +90,6 @@ eventHandler.bind("selectionChanged", function(event){
     //console.log(selectedNeighborhood);
     //update map vis
     myMapVis.wrangleData();
+
 
 });
--- js/radarChart.js
+++ js/radarChart.js
@@ -7,73 +7,61 @@
 
 /* Radar chart design created by Nadieh Bremer - VisualCinnamon.com */
 
-//////////////////////////////////////////////////////////////
-//////////////////////// Set-Up //////////////////////////////
-//////////////////////////////////////////////////////////////
+class RadarChart {
 
-var margin = {top: 100, right: 100, bottom: 100, left: 100},
-    width = Math.min(700, window.innerWidth - 10) - margin.left - margin.right,
-    height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
+
+    //constructor
+    constructor(parentElement, testData) {
+        this.parentElement = parentElement;
+        this.data = testData;
+
+
 
 //////////////////////////////////////////////////////////////
 ////////////////////////// Data //////////////////////////////
 //////////////////////////////////////////////////////////////
+//Superceded by moving it to main.js
 
-var data = [
-    [//iPhone
-        {axis:"Battery Life",value:0.22},
-        {axis:"Brand",value:0.28},
-        {axis:"Contract Cost",value:0.29},
-        {axis:"Design And Quality",value:0.17},
-        {axis:"Have Internet Connectivity",value:0.22},
-        {axis:"Large Screen",value:0.02},
-        {axis:"Price Of Device",value:0.21},
-        {axis:"To Be A Smartphone",value:0.50}
-    ],[//Samsung
-        {axis:"Battery Life",value:0.27},
-        {axis:"Brand",value:0.16},
-        {axis:"Contract Cost",value:0.35},
-        {axis:"Design And Quality",value:0.13},
-        {axis:"Have Internet Connectivity",value:0.20},
-        {axis:"Large Screen",value:0.13},
-        {axis:"Price Of Device",value:0.35},
-        {axis:"To Be A Smartphone",value:0.38}
-    ],[//Nokia Smartphone
-        {axis:"Battery Life",value:0.26},
-        {axis:"Brand",value:0.10},
-        {axis:"Contract Cost",value:0.30},
-        {axis:"Design And Quality",value:0.14},
-        {axis:"Have Internet Connectivity",value:0.22},
-        {axis:"Large Screen",value:0.04},
-        {axis:"Price Of Device",value:0.41},
-        {axis:"To Be A Smartphone",value:0.30}
-    ]
-];
 //////////////////////////////////////////////////////////////
 //////////////////// Draw the Chart //////////////////////////
 //////////////////////////////////////////////////////////////
 
-var color = d3.scaleOrdinal()
-    .range(["#EDC951","#CC333F","#00A0B0"]);
 
-var radarChartOptions = {
-    w: width,
-    h: height,
-    margin: margin,
-    maxValue: 0.5,
-    levels: 5,
-    roundStrokes: true,
-    color: color
-};
-//Call function to draw the Radar chart
-RadarChart(".radarChart", data, radarChartOptions);
 
-function RadarChart(id, data, options) {
-    var cfg = {
-        w: 600,				//Width of the circle
-        h: 600,				//Height of the circle
-        margin: {top: 20, right: 20, bottom: 20, left: 20}, //The margins of the SVG
-        levels: 3,				//How many levels or inner circles should there be drawn
+
+
+        //call initVis
+        this.RadarChart();
+    }
+
+    RadarChart() {
+
+    let vis = this;
+
+        vis.margin = {top: 100, right: 100, bottom: 100, left: 100};
+        //vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
+        //vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
+        vis.width = 500;
+        vis.height = 500;
+
+
+    vis.color = d3.scaleOrdinal()
+        .range(["#EDC951","#CC333F","#00A0B0"]);
+
+    vis.options = {
+            w: vis.width,
+            h: vis.height,
+            margin: vis.margin,
+            maxValue: 0.5,
+            levels: 5,
+            roundStrokes: true,
+            color: vis.color
+        };
+
+     vis.cfg = {
+        width: vis.width,
+        height: vis.height,
+        levels: 4,				//How many levels or inner circles should there be drawn
         maxValue: 0, 			//What is the value that the biggest circle will represent
         labelFactor: 1.25, 	//How much farther than the radius of the outer circle should the labels be placed
         wrapWidth: 60, 		//The number of pixels after which a label needs to be given a new line
@@ -85,20 +73,20 @@ function RadarChart(id, data, options) {
         color: d3.scaleOrdinal(d3.schemeCategory10)	//Color function
     };
 
-    //Put all of the options into a variable called cfg
-    if('undefined' !== typeof options){
-        for(var i in options){
-            if('undefined' !== typeof options[i]){ cfg[i] = options[i]; }
+    //Put all of the options into a variable called vis.cfg
+    if('undefined' !== typeof vis.options){
+        for(var i in vis.options){
+            if('undefined' !== typeof vis.options[i]){ vis.cfg[i] = vis.options[i]; }
         }//for i
     }//if
 
     //If the supplied maxValue is smaller than the actual one, replace by the max in the data
-    var maxValue = Math.max(cfg.maxValue, d3.max(data, function(i){return d3.max(i.map(function(o){return o.value;}))}));
+    var maxValue = Math.max(vis.cfg.maxValue, d3.max(vis.data, function(i){return d3.max(i.map(function(o){return o.value;}))}));
 
-    var allAxis = (data[0].map(function(i, j){return i.axis})),	//Names of each axis
-        total = allAxis.length,					//The number of different axes
-        radius = Math.min(cfg.w/2, cfg.h/2), 	//Radius of the outermost circle
-        Format = d3.format('%'),			 	//Percentage formatting
+    var allAxis = (vis.data[0].map(function(i, j){return i.axis})),	//Names of each axis
+        total = allAxis.length, //The number of different axes
+        radius = Math.min(vis.cfg.width/2, vis.cfg.height/2), 	//Radius of the outermost circle
+        Format = d3.format('.0%'),			 	//Percentage formatting
         angleSlice = Math.PI * 2 / total;		//The width in radians of each "slice"
 
     //Scale for the radius
@@ -111,16 +99,18 @@ function RadarChart(id, data, options) {
     /////////////////////////////////////////////////////////
 
     //Remove whatever chart with the same id/class was present before
-    d3.select(id).select("svg").remove();
+    d3.select("#" + vis.parentElement).select("svg").remove();
 
     //Initiate the radar chart SVG
-    var svg = d3.select(id).append("svg")
-        .attr("width",  cfg.w + cfg.margin.left + cfg.margin.right)
-        .attr("height", cfg.h + cfg.margin.top + cfg.margin.bottom)
-        .attr("class", "radar"+id);
+    var svg = d3.select("#" + vis.parentElement).append("svg")
+        //.attr("width",  vis.width + vis.margin.left + vis.margin.right)
+        .attr("width",  1000)
+        //.attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+        .attr("height", 1000)
+        .attr("class", "radar");
     //Append a g element
     var g = svg.append("g")
-        .attr("transform", "translate(" + (cfg.w/2 + cfg.margin.left) + "," + (cfg.h/2 + cfg.margin.top) + ")");
+        .attr("transform", "translate(500,400)");
 
     /////////////////////////////////////////////////////////
     ////////// Glow filter for some extra pizzazz ///////////
@@ -142,27 +132,28 @@ function RadarChart(id, data, options) {
 
     //Draw the background circles
     axisGrid.selectAll(".levels")
-        .data(d3.range(1,(cfg.levels+1)).reverse())
+        .data(d3.range(1,(vis.cfg.levels+1)).reverse())
         .enter()
         .append("circle")
         .attr("class", "gridCircle")
-        .attr("r", function(d, i){return radius/cfg.levels*d;})
+        .attr("r", function(d, i){
+            return radius/vis.cfg.levels*(d);})
         .style("fill", "#CDCDCD")
         .style("stroke", "#CDCDCD")
-        .style("fill-opacity", cfg.opacityCircles)
+        .style("fill-opacity", vis.cfg.opacityCircles)
         .style("filter" , "url(#glow)");
 
     //Text indicating at what % each level is
     axisGrid.selectAll(".axisLabel")
-        .data(d3.range(1,(cfg.levels+1)).reverse())
+        .data(d3.range(1,(vis.cfg.levels+1)).reverse())
         .enter().append("text")
         .attr("class", "axisLabel")
         .attr("x", 4)
-        .attr("y", function(d){return -d*radius/cfg.levels;})
+        .attr("y", function(d){return -d*radius/vis.cfg.levels;})
         .attr("dy", "0.4em")
         .style("font-size", "10px")
-        .attr("fill", "#737373")
-        .text(function(d,i) { return Format(maxValue * d/cfg.levels); });
+        .attr("fill", "white")
+        .text(function(d,i) { return Format(maxValue * d/vis.cfg.levels) });
 
     /////////////////////////////////////////////////////////
     //////////////////// Draw the axes //////////////////////
@@ -190,10 +181,10 @@ function RadarChart(id, data, options) {
         .style("font-size", "11px")
         .attr("text-anchor", "middle")
         .attr("dy", "0.35em")
-        .attr("x", function(d, i){ return rScale(maxValue * cfg.labelFactor) * Math.cos(angleSlice*i - Math.PI/2); })
-        .attr("y", function(d, i){ return rScale(maxValue * cfg.labelFactor) * Math.sin(angleSlice*i - Math.PI/2); })
+        .attr("x", function(d, i){ return rScale(maxValue * vis.cfg.labelFactor) * Math.cos(angleSlice*i - Math.PI/2); })
+        .attr("y", function(d, i){ return rScale(maxValue * vis.cfg.labelFactor) * Math.sin(angleSlice*i - Math.PI/2); })
         .text(function(d){return d})
-        .call(wrap, cfg.wrapWidth);
+        .call(wrap, vis.cfg.wrapWidth);
 
     /////////////////////////////////////////////////////////
     ///////////// Draw the radar chart blobs ////////////////
@@ -205,13 +196,13 @@ function RadarChart(id, data, options) {
         .radius(function(d) { return rScale(d.value); })
         .angle(function(d,i) {	return i*angleSlice; });
 
-    if(cfg.roundStrokes) {
+    if(vis.cfg.roundStrokes) {
         radarLine.curve(d3.curveCardinalClosed);
     }
 
     //Create a wrapper for the blobs
     var blobWrapper = g.selectAll(".radarWrapper")
-        .data(data)
+        .data(vis.data)
         .enter().append("g")
         .attr("class", "radarWrapper");
 
@@ -220,8 +211,8 @@ function RadarChart(id, data, options) {
         .append("path")
         .attr("class", "radarArea")
         .attr("d", function(d,i) { return radarLine(d); })
-        .style("fill", function(d,i) { return cfg.color(i); })
-        .style("fill-opacity", cfg.opacityArea)
+        .style("fill", function(d,i) { return vis.cfg.color(i); })
+        .style("fill-opacity", vis.cfg.opacityArea)
         .on('mouseover', function (d,i){
             //Dim all blobs
             d3.selectAll(".radarArea")
@@ -236,15 +227,15 @@ function RadarChart(id, data, options) {
             //Bring back all blobs
             d3.selectAll(".radarArea")
                 .transition().duration(200)
-                .style("fill-opacity", cfg.opacityArea);
+                .style("fill-opacity", vis.cfg.opacityArea);
         });
 
     //Create the outlines
     blobWrapper.append("path")
         .attr("class", "radarStroke")
         .attr("d", function(d,i) { return radarLine(d); })
-        .style("stroke-width", cfg.strokeWidth + "px")
-        .style("stroke", function(d,i) { return cfg.color(i); })
+        .style("stroke-width", vis.cfg.strokeWidth + "px")
+        .style("stroke", function(d,i) { return vis.cfg.color(i); })
         .style("fill", "none")
         .style("filter" , "url(#glow)");
 
@@ -253,10 +244,10 @@ function RadarChart(id, data, options) {
         .data(function(d,i) { return d; })
         .enter().append("circle")
         .attr("class", "radarCircle")
-        .attr("r", cfg.dotRadius)
+        .attr("r", vis.cfg.dotRadius)
         .attr("cx", function(d,i){ return rScale(d.value) * Math.cos(angleSlice*i - Math.PI/2); })
         .attr("cy", function(d,i){ return rScale(d.value) * Math.sin(angleSlice*i - Math.PI/2); })
-        .style("fill", function(d,i,j) { return cfg.color(j); })
+        .style("fill", function(d,i,j) { return vis.cfg.color(j); })
         .style("fill-opacity", 0.8);
 
     /////////////////////////////////////////////////////////
@@ -265,30 +256,33 @@ function RadarChart(id, data, options) {
 
     //Wrapper for the invisible circles on top
     var blobCircleWrapper = g.selectAll(".radarCircleWrapper")
-        .data(data)
+        .data(vis.data)
         .enter().append("g")
         .attr("class", "radarCircleWrapper");
 
     //Append a set of invisible circles on top for the mouseover pop-up
     blobCircleWrapper.selectAll(".radarInvisibleCircle")
-        .data(function(d,i) { return d; })
+        .data(function(d,i) {return d; })
         .enter().append("circle")
         .attr("class", "radarInvisibleCircle")
-        .attr("r", cfg.dotRadius*1.5)
+        .attr("r", vis.cfg.dotRadius*1.5)
         .attr("cx", function(d,i){ return rScale(d.value) * Math.cos(angleSlice*i - Math.PI/2); })
         .attr("cy", function(d,i){ return rScale(d.value) * Math.sin(angleSlice*i - Math.PI/2); })
         .style("fill", "none")
         .style("pointer-events", "all")
         .on("mouseover", function(d,i) {
-            newX =  parseFloat(d3.select(this).attr('cx')) - 10;
-            newY =  parseFloat(d3.select(this).attr('cy')) - 10;
+             vis.newX =  parseFloat(d3.select(this).attr('cx')) - 10;
+             vis.newY =  parseFloat(d3.select(this).attr('cy')) - 10;
+
+            console.log(i);
 
             tooltip
-                .attr('x', newX)
-                .attr('y', newY)
-                .text(Format(d.value))
+                .attr('x', vis.newX)
+                .attr('y', vis.newY)
+                .text(Format(i.value))
                 .transition().duration(200)
-                .style('opacity', 1);
+                .style('opacity', 1)
+                .style("fill",'white');
         })
         .on("mouseout", function(){
             tooltip.transition().duration(200)
@@ -332,4 +326,4 @@ function RadarChart(id, data, options) {
         });
     }//wrap
 
-}//RadarChart
+}}//RadarChart
